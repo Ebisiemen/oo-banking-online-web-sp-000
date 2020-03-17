160cce517cc01ef7c6be35dbc03dc0f6f6e98d27
Ebisiemen Ajayi <ebisiemen@gmail.com>
2020-03-17 16:15:21 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,9 @@
 class BankAccount
-
+  attr_reader :name 
+  attr_writer :balance, :status 
+   def initialize(name, balance = 1000, status = 'open')
+     @name = name 
+     @balance = 1000
+     @status = 'open'
+  end    
 end
